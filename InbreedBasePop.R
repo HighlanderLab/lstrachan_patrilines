f12c34d13c67dcc15891bec9ecd7565b4893d318
janaobsteter <obsteter.jana@gmail.com>
2026-01-06 13:39:12 +0100

    Inbreeding base pop

---
+++ InbreedBasePop.R
@@ -0,0 +1,358 @@
+# Clean workspace
+rm(list = ls())
+
+#library
+library(AlphaSimR)
+library(SIMplyBee)
+library(readr)
+library(genio)
+library(AGHmatrix)
+library(tidyr)
+library(tibble)
+library(dplyr)
+library(ggplot2)
+library(future)
+library(future.apply)
+library(doParallel)
+
+# Create/download the founder genomes from SIMplyBee
+founderGenomes <- simulateHoneyBeeGenomes(nCar = 30,
+                                          nChr = 16,
+                                          nSegSites = 1000,
+                                          Ne = 2000)
+
+save(founderGenomes, file = "~/Documents/1Projects/HoneybeeParentage/FounderGenome_ParentagePaper.RData")
+load("~/Documents/1Projects/HoneybeeParentage/FounderGenome_ParentagePaper.RData")
+load("/home/jana/Documents/1Projects/Honeybee_BeeKini/FounderGenomes_ThreePop_16chr.RData")
+#load("~/Desktop/Slovenia data/Attempt2/founderGenomes_10ind.Rdata")
+
+# Try a different genom simulation
+founderGenomes = quickHaplo(nInd = 100, nChr=16, segSites = 1000)
+founderGenomes = runMacs(nInd = 30, nChr = 16, segSites = 1000,
+                         species = "CATTLE")
+
+
+#Set up the SP ####
+SP <- SimParamBee$new(founderGenomes, csdChr = ifelse(16 >= 3, 3, 1), nCsdAlleles = 128)
+# Without the csd
+SP <- SimParamBee$new(founderGenomes, csdChr = NULL)
+
+SP$nWorkers <- 30
+# Track the pedigree
+SP$setTrackPed(TRUE)
+# Track the recombination
+SP$setTrackRec(TRUE)
+
+SP$addSnpChip(nSnpPerChr = 10) # CHANGE THE SNP SIZE HERE!!!!! (ADD A FEW EXTRA FOR QC THEN SAMPLE THE CORRECT SIZES)# define csd chromomsome
+csdChr <- SP$csdChr
+
+
+# Add traitsNULL# Add traits - taken from the QuantGen vignettte
+mean <- c(20, 0)
+varA <- c(1, 1 / SP$nWorkers)
+corA <- matrix(data = c( 1.0, -0.5,
+                         -0.5,  1.0), nrow = 2, byrow = TRUE)
+SP$addTraitA(nQtlPerChr = 100, mean = mean, var = varA, corA = corA,
+             name = c("queenTrait", "workersTrait"))
+
+varE <- c(3, 3 / SP$nWorkers)
+# TODO: what is a reasonable environmental correlation between queen and worker effects?
+corE <- matrix(data = c(1.0, 0.3,
+                        0.3, 1.0), nrow = 2, byrow = TRUE)
+SP$setVarE(varE = varE, corE = corE)
+
+##################################################################################
+##################################################################################
+
+
+# Check the relatedness in the real data
+realGeno_meta = read.csv("/home/jana/Documents/1Projects/HoneybeeParentage/data/SNP_samples_2022.csv")
+realGeno = read.table("/home/jana/Documents/1Projects/HoneybeeParentage/data/newPat_CleanIndsMarkers.raw",
+                      header=T)
+table(realGeno$IID %in% realGeno_meta$snp_id)
+realGeno_meta = realGeno_meta %>% dplyr::filter(snp_id %in% realGeno$IID)
+table(realGeno_meta$biotype)
+
+realGeno_IID = realGeno$IID
+realGeno_geno = realGeno[, 7:ncol(realGeno)]
+realGeno_snpnames = colnames(realGeno)
+realGeno_geno[is.na(realGeno_geno)] = -9
+rownames(realGeno_geno) = realGeno_IID
+realGeno_G = as.data.frame(Gmatrix(SNPmatrix = as.matrix(realGeno_geno),
+                                   missing = -9,
+                                   ploidy = 2)) %>%
+  rownames_to_column("IID") %>%
+  pivot_longer(-IID, names_to = "IID2")
+
+realGeno_G %>% filter(IID == IID2) %>%
+  ggplot() + geom_histogram(aes(x = value - 1))
+
+realGeno_G %>% filter(IID == IID2) %>% summarise(mean(value - 1))
+
+#Only queens
+realGeno_G %>% filter(IID == IID2) %>% filter(IID %in% realGeno_meta$snp_id[realGeno_meta$biotype == "queen"]) %>%
+  ggplot() + geom_histogram(aes(x = value - 1))
+
+realGeno_G %>% filter(IID == IID2) %>% filter(IID %in% realGeno_meta$snp_id[realGeno_meta$biotype == "queen"]) %>%
+  summarise(mean(value-1))
+
+#Only DPQ
+realGeno_G %>% filter(IID == IID2) %>% filter(IID %in% realGeno_meta$snp_id[realGeno_meta$biotype == "dpc"]) %>%
+  ggplot() + geom_histogram(aes(x = value - 1))
+
+realGeno_G %>% filter(IID == IID2) %>% filter(IID %in% realGeno_meta$snp_id[realGeno_meta$biotype == "dpc"]) %>%
+  summarise(mean(value-1))
+
+realGeno_G$Pop1 = sapply(realGeno_G$IID, FUN = function(x) realGeno_meta$biotype[realGeno_meta$snp_id == x])
+realGeno_G$Pop2 = sapply(realGeno_G$IID2, FUN = function(x) realGeno_meta$biotype[realGeno_meta$snp_id == x])
+realGeno_G$Data = "Real"
+realGeno_G$Gen = "Real"
+realGeno_G$Pop = ifelse(realGeno_G$Pop1 == "queen" & realGeno_G$Pop2 == "queen", "queen",
+                        ifelse(realGeno_G$Pop1 == "dpc" & realGeno_G$Pop2 == "dpc", "dpc",
+                               ifelse(realGeno_G$Pop1 == "worker" & realGeno_G$Pop2 == "worker", "worker", "across")))
+realGeno_G$Mating = "Real"
+
+inbreeding = data.frame()
+inbreeding = rbind(inbreeding, realGeno_G)
+##################################################################################
+##################################################################################
+
+# Create X rounds of inbreeding, check relatedness after each - NON BEE VERSION
+nQueens = data.frame()
+SP$nThreads = detectCores()
+plan(multisession, workers = SP$nThreads)
+
+for (gen in 1:10) {
+  print(paste0("Base generation is ", gen))
+  if (gen == 1) {
+    #Create base population#realGenoCreate base population
+    #basePop = newPop(founderGenomes[1:30])
+    basePop = newPop(founderGenomes[1:10])
+    print(paste0(nInd(basePop), " base queens."))
+    baseAF = calcBeeAlleleFreq(pullSegSiteGeno(basePop), sex = rep("F", basePop@nInd))
+  } else {
+    # A round of inbreeding
+    basePop = randCross(basePop[1:10], nCrosses = basePop[1:10]@nInd)
+  }
+  # Compute relatedness
+  basePop_geno = pullSegSiteGeno(basePop)
+  basePop_G= as.data.frame(Gmatrix(SNPmatrix = basePop_geno,
+                                   ploidy = 2,
+                                   maf = baseAF)) %>%
+    rownames_to_column("IID") %>%
+    pivot_longer(-IID, names_to = "IID2")
+
+  basePop_G$Pop1 = paste0("SimFounders", gen)
+  basePop_G$Pop2 = paste0("SimFounders", gen)
+  basePop_G$Data = "Sim"
+  basePop_G$Gen = gen
+  basePop_G$Pop = "queen"
+  basePop_G$Mating = "NonBee_version"
+  inbreeding = rbind(inbreeding, basePop_G)
+  nQueens = rbind(nQueens, data.frame(Gen = gen, nQueens = basePop@nInd, Mating = "NonBee"))
+
+  if (gen == 10) {
+    # Create bee virgin queens from pop
+    founderPop = new(Class = "MapPop",
+                     nInd = basePop@nInd,
+                     nChr = basePop@nChr,
+                     ploidy = basePop@ploidy,
+                     nLoci = basePop@nLoci,
+                     geno = basePop@geno,
+                     genMap = founderGenomes@genMap,
+                     centromere = founderGenomes@centromere,
+                     inbred = founderGenomes@inbred)
+
+    # Create DPQ and inspect their relatedness
+    bee_basePop = createVirginQueens(founderPop)
+    pFathers <- nFathersPoisson
+    nDronesPerQueen = 50
+    drones <- createDrones(bee_basePop[1], nInd = nDronesPerQueen)
+    fathers <- pullDroneGroupsFromDCA(drones, n = 1, nDrones = pFathers)
+
+    #Create a colony and cross it
+    colony1 <- createColony(x = bee_basePop[2])
+    colony1 <- cross(colony1, drones = fathers[[1]])
+
+
+
+    #create Mating station Drones where the dpcs are
+    DPQs <- createVirginQueens(colony1, nInd = 4)
+
+    # Compute relatedness of DPQs
+    DPQ_geno = pullSegSiteGeno(DPQs)
+    DPQ_G= as.data.frame(Gmatrix(SNPmatrix = DPQ_geno,
+                                 ploidy = 2,
+                                 maf = baseAF)) %>%
+      rownames_to_column("IID") %>%
+      pivot_longer(-IID, names_to = "IID2")
+
+    DPQ_G$Pop1 = paste0("SimFounders", gen)
+    DPQ_G$Pop2 = paste0("SimFounders", gen)
+    DPQ_G$Data = "Sim"
+    DPQ_G$Gen = gen
+    DPQ_G$Pop <- "dpc"
+    DPQ_G$Mating = "NonBee_version"
+    inbreeding = rbind(inbreeding, DPQ_G)
+  }
+}
+
+
+#### BEE VERSION
+founderGenomes = founderGenomes[1:30]
+for (gen in 1:10) {
+  print(paste0("Base generation is ", gen))
+  if (gen == 1) {
+    #Create base population#realGenoCreate base population
+    #basePop <- createVirginQueens(founderGenomes[1:30])
+    basePop <- createVirginQueens(founderGenomes)
+    baseAF = calcBeeAlleleFreq(pullSegSiteGeno(basePop), sex = rep("F", basePop@nInd))
+  } else {
+    # A round of inbreeding
+    # This is bee version
+    print("Creating drones")
+    drones = createDrones(basePop, nInd = 100)
+    print("Crossing queens")
+    queens = cross(basePop, drones = drones, crossPlan = "create")
+    print("Create colonies")
+    baseColonies <- createMultiColony(queens)
+    print("Merging pops")
+    basePop = mergePops(createVirginQueens(baseColonies, nInd = 1))
+  }
+
+  # Compute relatedness
+  basePop_geno = pullSegSiteGeno(basePop)
+  basePop_G= as.data.frame(Gmatrix(SNPmatrix = basePop_geno,
+                                   ploidy = 2,
+                                   maf = baseAF)) %>%
+    rownames_to_column("IID") %>%
+    pivot_longer(-IID, names_to = "IID2")
+
+  basePop_G$Pop1 = paste0("queen")
+  basePop_G$Pop2 = paste0("queen")
+  basePop_G$Data = "Sim"
+  basePop_G$Gen = gen
+  basePop_G$Pop <- "queen"
+  basePop_G$Mating = "Bee_version"
+  inbreeding = rbind(inbreeding, basePop_G)
+  nQueens = rbind(nQueens, data.frame(Gen = gen, nQueens = basePop@nInd, Mating = "Bee"))
+
+  if (gen == 10) {
+    # Create DPQ and inspect their relatedness
+    pFathers <- nFathersPoisson
+    nDronesPerQueen = 50
+    drones <- createDrones(basePop[1], nInd = nDronesPerQueen)
+    fathers <- pullDroneGroupsFromDCA(drones, n = 1, nDrones = pFathers)
+
+    #Create a colony and cross it
+    colony1 <- createColony(x = basePop[2])
+    colony1 <- cross(colony1, drones = fathers[[1]])
+
+    #create Mating station Drones where the dpcs are
+    DPQs <- createVirginQueens(colony1, nInd = 4)
+
+    # Compute relatedness of DPQs
+    DPQ_geno = pullSegSiteGeno(DPQs)
+    DPQ_G= as.data.frame(Gmatrix(SNPmatrix = DPQ_geno,
+                                 ploidy = 2,
+                                 maf = baseAF)) %>%
+      rownames_to_column("IID") %>%
+      pivot_longer(-IID, names_to = "IID2")
+
+    DPQ_G$Pop1 = paste0("dpc")
+    DPQ_G$Pop2 = paste0("dpc")
+    DPQ_G$Data = "Sim"
+    DPQ_G$Gen = gen
+    DPQ_G$Pop <- "dpc"
+    DPQ_G$Mating = "Bee_version"
+    inbreeding = rbind(inbreeding, DPQ_G)
+  }
+}
+
+save.image(file = "200000NE_HBGenome_CsdOn_20gen.RData")
+save.image(file = "10000NE_HBGenome_CsdOn_20gen.RData")
+save.image(file = "2000NE_HBGenome_CsdOn_20gen.RData")
+save.image(file = "CattleGenome_CsdOn_20gen.RData")
+
+rm(list = ls())
+load("10000NE_HBGenome_CsdOn_20gen.RData")
+load("2000NE_HBGenome_CsdOn_20gen.RData")
+load("CattleGenome_CsdOn_20gen.RData")
+##################################################################################
+# PLOT
+##################################################################################
+meanRel_queen = inbreeding %>%
+  dplyr::mutate(SelfRel = ifelse((IID == IID2), "SelfRel", "Rel")) %>%
+  filter(Data == "Sim" | (Data == "Real" & Pop %in% c("queen", "dpc"))) %>%
+  group_by(Pop, SelfRel, Data, Gen, Mating) %>%
+  summarise(meanRel = mean(value), varRel = var(value))
+
+inbreeding$PopGen <- paste0(inbreeding$Pop, inbreeding$Gen)
+meanRel_queen$PopGen <- paste0(meanRel_queen$Pop, meanRel_queen$Gen)
+meanRel_queen %>% ggplot() +
+  geom_col(aes(x = PopGen, y = meanRel, fill = SelfRel), position = "dodge") +
+  facet_grid(. ~ Mating)
+
+
+plot_step=5
+# Plot for self relatedness
+# Real data
+inbreeding %>%
+  dplyr::mutate(SelfRel = (IID == IID2)) %>%
+  filter(Data == "Real" & Pop %in% c("queen", "dpc")) %>%
+  filter(SelfRel) %>%
+  ggplot() + geom_histogram(aes(x = value)) +
+  theme_bw(base_size=16) +
+  facet_grid(. ~ Pop)
+
+
+# Simulated
+inbreeding %>%
+  dplyr::mutate(SelfRel = (IID == IID2)) %>%
+  filter(Data == "Sim") %>%
+  filter(SelfRel) %>%
+  #filter(Gen %in% c(paste0(seq(1, 50, plot_step)))) %>%
+  ggplot() + geom_histogram(aes(x = value)) +
+  facet_grid(rows = vars(PopGen), cols = vars(Mating)) +
+  theme_bw(base_size=16)
+
+# Plot for across relatedness - queens
+#Real
+#Real
+inbreeding %>%
+  dplyr::mutate(SelfRel = (IID == IID2)) %>%
+  filter((Data == "Real" & Pop %in% c("queen", "dpc"))) %>%
+  filter(SelfRel != "SelfRel") %>%
+  ggplot() + geom_histogram(aes(x = value)) +
+  facet_wrap(. ~ Pop, nrow = 2) +
+  theme_bw(base_size=16)
+
+# Simulated
+inbreeding %>%
+  dplyr::mutate(SelfRel = (IID == IID2)) %>%
+  filter(Data == "Sim") %>%
+  filter(!SelfRel) %>%
+  #filter(Gen %in% seq(1, 50, plot_step)) %>%
+  ggplot() + geom_histogram(aes(x = value), bins = 100) +
+  facet_grid(rows = vars(PopGen), cols = vars(Mating)) +
+
+  theme_bw(base_size=16)
+
+meanRel_queen %>%
+  filter(Data == "Sim") %>%
+  filter(Pop == "queen") %>%
+  ggplot() +
+  geom_line(aes(x = as.numeric(Gen), y = meanRel, colour = Mating)) +
+  facet_wrap(. ~ SelfRel, scales="free", nrow=2) +
+  xlab("Mean relatedeness") + ylab("Generation")
+
+meanRel_queen %>% filter(Gen %in%  c(10, "Real")) %>%
+  dplyr::mutate(PlotPop = paste0(Data, "_", Pop)) %>%
+  ggplot(aes(x = PlotPop, y = meanRel))+
+  geom_col() +
+  facet_grid(. ~SelfRel)
+
+
+ggplot(data = nQueens) + geom_line(aes(x = as.numeric(Gen), y =nQueens, colour = Mating)) +
+  ylim(c(0, max(nQueens$nQueens)))
+
